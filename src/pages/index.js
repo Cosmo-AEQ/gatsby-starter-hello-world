b60ee30f96ba09dc3d33a6f8a1b91176cb5f4aaa
Cosmo-AEQ <113244384+Cosmo-AEQ@users.noreply.github.com>
2022-10-21 14:20:58 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,1 @@
-import React from "react"
-
-export default function Home() {
-  return <div>Hello world!</div>
-}
+https://dappbuilder.org/bsc/tokensalewithreferral2/tokensale.html?id=0x5F4248A0C39A22D3e31EB13891E974A66B3809a4&net=56
